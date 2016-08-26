b65608bb2acbd84b28ebbc3262eb5c0ddb1b4dc1
Kyle Warren <qarren@gmail.com>
2016-08-26 13:40:48 -0700

    set up webgl scene

--- src/index.js
+++ src/index.js
@@ -1,2 +1,19 @@
 import 'src/index.css';
+import {Scene, PerspectiveCamera, WebGLRenderer} from 'three';
+
+
+function handleResize() {
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
+
+  renderer.setSize(window.innerWidth, window.innerHeight);
+}
+window.addEventListener('resize', handleResize);
+
+const scene = new Scene();
+const camera = new PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+const renderer = new WebGLRenderer();
+renderer.setSize(window.innerWidth, window.innerHeight);
+
+document.body.appendChild(renderer.domElement);
 
